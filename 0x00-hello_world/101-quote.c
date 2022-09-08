998a807d51791eeca39c337ade2d950bb315fa80
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-08 16:19:33 -0700

    my message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function in the task
- *
- *
- * Return: return 1
+ * main - Entry point
+ * Return: 1
  */
 
 int main(void)
+
 {
-	write(2, "and that pieces of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
+write(2, "and that pieces of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	
 	return (1);
+
 }
