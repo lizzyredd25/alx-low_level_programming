d481b8424d5922ee5ee2f6e910e6255a117820a4
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-08 16:32:25 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: 1
@@ -8,7 +9,7 @@ int main(void)
 
 {
 
-write(2, "and that pieces of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that pieces of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	
 	return (1);
 
