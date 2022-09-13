a4ce3057e6cb72a175b80de09a35b45502bc3561
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-13 14:26:18 -0700

    7 last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit 
+ * print_last_digit - prints the last digit
  * @n: the int to extract the last digit from
  * Return: value of last digit
  */
@@ -11,8 +11,8 @@ int r;
 if (n < 0)
 n = -n;
 r = n % 10;
-if (r <0)
+if (r < 0)
 r = -r;
-_putchar(r +'0');
+_putchar(r + '0');
 return (r);
 }
