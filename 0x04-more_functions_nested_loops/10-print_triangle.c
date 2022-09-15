a70d9869fa05fb0152d831a9356cec31d6835535
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-15 14:05:05 -0700

    file 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a traingle, followed by a new line
+ * @size: size of the triangle
+ * Return: 0
+ */
+void print_trianglr(int size)
+{
+	int i, k, p;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	p = size;
+	for (i = 1; i <= size; i++)
+	{
+		for (k = 1; k <= size; k++)
+		{
+			if (k < p)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		p--;
+		_putchar('\n');
+	}
+}
