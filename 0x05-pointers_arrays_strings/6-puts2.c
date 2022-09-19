a2b605f7b575b3897cb8cf6625fad4bf103f31ac
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-19 06:03:46 -0700

    6 puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: pointer to string str
+ */
+void puts2(char *str)
+{
+	int i, slen;
+
+	slen = strlen(str);
+	for (i = 0; i < slen; i +=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
