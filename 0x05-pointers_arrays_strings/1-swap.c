559601ec2397f1d4eab6c5764cf078a7097d85c6
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-19 05:26:18 -0700

    1 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of 2 integers
+ * @a: pointer to integer 1
+ * @b: pointer to integer 1
+ */
+void swa_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
