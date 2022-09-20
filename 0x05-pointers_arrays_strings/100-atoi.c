9b10852f8ca0b946aff660a69c1822a39de99b12
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-19 18:11:37 -0700

    new task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: pointer to string s
+ * Return: the number for string s
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
