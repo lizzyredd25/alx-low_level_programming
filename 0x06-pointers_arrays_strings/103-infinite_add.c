cae4b8ef3a99270130ae1eef6127827b672289b6
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-21 15:01:00 -0700

    infinity crime

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,7 @@
  * infinite_add - print integer using _putchar
  * @n: number to print
  */
-char @infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	return ('s');
 }
