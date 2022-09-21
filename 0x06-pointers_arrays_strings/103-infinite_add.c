9f2ddbc2d8f2c5190a31e4b01567bc9b65ec022e
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-21 15:02:25 -0700

    nawa oo

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,27 @@
 #include "main.h"
 
 /**
- * infinite_add - print integer using _putchar
- * @n: number to print
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting strring
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *rot13(char *s)
 {
-	return ('s');
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+	    }
+	}
+	return (s);
 }
