cfa16a8539c95e426cd6677bcf21d035ac268e9c
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-21 14:59:26 -0700

    infinity wars

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * infinite_add - print integer using _putchar
+ * @n: number to print
+ */
+char @infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return ('s');
+}
