4cdf72ea7c70c7d215e90fb22c499b2b735e0914
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-26 09:35:08 -0700

    pointer value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * set_string - sets the value of a pointer to a character
+ * @s: array of square matrix
+ * @to: size of array a
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
