3f62b697feb82a47efbeeac62f985a8a2d2173c7
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-26 09:12:52 -0700

    substring n

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
  * _strstr - locates a substring
  * :haystack: string to search substring in
  * @needle: substring to serach for
- * Return: pointer to tghe beginning of the located substring,
+ * Return: pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
