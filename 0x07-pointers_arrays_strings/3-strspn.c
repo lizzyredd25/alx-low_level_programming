921c9be488d611f9e435fc96783e76bb38fd05fc
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-26 08:57:55 -0700

    n prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,8 @@
  * _strspn - gets the length of a prefix substring
  * @s: destination memory area
  * @accept: bytes of the memory area
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: number of bytes in the initial segment of s which consist 
+ * only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
