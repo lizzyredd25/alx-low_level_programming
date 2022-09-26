6b04ab85167923c610a4c1d3feb072ba265f828f
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-26 08:36:27 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _memset - fills memory with a constant byte
+ * @s: destiny str
+ * @b: constant byte
+ * @n: bytes of the memory area pointed to
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n)
+}
