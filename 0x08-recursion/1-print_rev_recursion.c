53685956b800ef4c2950e783770f1156d0ac60fc
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-28 06:33:41 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ * Return: 0
+ */
+void _print_rev_recursion(chars *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
