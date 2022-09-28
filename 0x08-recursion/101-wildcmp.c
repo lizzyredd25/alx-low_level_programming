40115317e73e63977ab1759506d8d01dc56e985f
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-09-28 07:44:57 -0700

    compares strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,38 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-/**
- * my_wildcmp - compares two strings
- * @s1: string to check
- * @s2: string to check
- * @i: s1 index
- * @j: s2 index
- * Return: 1 if the strings can be considered identical, otherwise return 0
- */
-int my_wildcmp(char *s1, char *s2, int i, int j)
-{
-	int l;
-
-	l = strlen(s1) > strlen(s2)? strlen(s1) : strlen(s2);
-	if (s1[i] == s2[j])
-		return (1);
-	if (s1[i] != s2[j])
-		return (0);
-	return (my_wildcmp(s1, s2, i + 1, j - 1));
-}
+#include "main.h"
 
 /**
- * is_palindrome - compares two strings
- * @s1: string to check
- * @s2: string to check
- * Return: 1 if the strings can be considered identical, otherwise return 0
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	int i, j;
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
-	if (s1 == s2)
-		return (1);
-	i = 0;
-	j = 0;
-	return (my_wildcmp(s1, s2, i, j));
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
