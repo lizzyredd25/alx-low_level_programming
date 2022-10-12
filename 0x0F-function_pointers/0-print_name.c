38e9b991a94e7a3f21edadbd5634060affc7e5e6
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-10-12 08:24:36 -0700

    alone

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 /**
  * print_name - prints a name using a function
  * @name: name to print
- * @f: function to print the name 
+ * @f: function to print the name
  * Return: 0
  */
 void print_name(char *name, void (*f)(char *))
