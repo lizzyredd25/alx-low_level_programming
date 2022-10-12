6099f1c5f36f19154289d9e370e3e1f3697e898c
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-10-12 08:35:49 -0700

    hot

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ * int_index - searches for an integer using a function
+ * @array: array of int to iterate
+ * @size: size of the array
+ * @cmp: function to use
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int o;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (o = 0; o < size; o++)
+		{
+			if (cmp(array[o] != 0)
+				return (o);
+		}
+	}
+	return (-1);
+}
